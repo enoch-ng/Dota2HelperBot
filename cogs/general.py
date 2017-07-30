082e7c05cc131baf3e663ddefa1dd52c85f5b56e
Enoch Ng <ewnoch@gmail.com>
2017-07-30 18:26:14 -0500

    Slight edit to FAQ

--- cogs/general.py
+++ cogs/general.py
@@ -204,7 +204,7 @@ class General:
 		emb.set_author(name = "Frequently Asked Questions")
 		emb.add_field(name = "What's with the nicknames? Can I set my own nickname for the bot?", value = "If the `autochangename` setting is on, the bot will automatically change its name periodically the name of a random Dota 2 bot. To set your own nickname for the bot, disable `autochangename` and then right-click the bot and select \"Change Nickname\".")
 		emb.add_field(name = "Is The International already on the bot's list or do I need to add it?", value = "It should already be there if you're using my instance of the bot. You can check by looking for \"5401\" in the `%sleagues` command." % self.bot.get_prefix())
-		emb.add_field(name = "Will you periodically add new leagues to the bot?", value = "I will on my instance of the bot. If you're running your own instance of the bot, you'll have to add new leagues through the `%saddleague` command." % self.bot.get_prefix())
+		emb.add_field(name = "Will you periodically add new leagues to the bot?", value = "I will on my instance of the bot. If you're running your own instance of the bot, you'll have to add new leagues through the `%saddleague` command or by editing the settings.json file." % self.bot.get_prefix())
 		emb.add_field(name = "How do I find out what ID a tournament comes under?", value = "You can get league IDs by calling Valve's API (https://api.steampowered.com/IDOTA2Match_570/GetLeagueListing/v1/?key=YOUR_API_KEY). In the future I hope to have some way to let people know the league IDs of upcoming tournaments more easily.")
 
 		try:
